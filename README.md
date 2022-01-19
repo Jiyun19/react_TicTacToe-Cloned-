834f95864a4c602b5af5ad4100c7d206ca18e1ce
Jiyun19 <jiyunlee219@gmail.com>
2022-01-20 07:55:38 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-yfqrfp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-yfqrfp)
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import "./style.css";
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {}
+   
